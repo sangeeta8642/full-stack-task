0f051e834f4cf5052f703ada5fd6f2bc432f984e
sangeeta8642 <sangeetaperagani8642@gmail.com>
2025-05-24 23:24:51 +0530

    form-view

--- src/app/pages/form-view/form-view.component.ts
+++ src/app/pages/form-view/form-view.component.ts
@@ -1,175 +1,382 @@
+// import { Component } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { Store } from '@ngrx/store';
+// import { formFields } from 'src/app/config/form-config';
+// import { getAllBoards } from 'src/app/ngrx/boards/boards.selector';
+// import { getAllReleases } from 'src/app/ngrx/releases/releases.selectors';
+// import { getAllEpics } from 'src/app/ngrx/epics/epics.selectors';
+// import { getAllSprints } from 'src/app/ngrx/sprints/sprints.selectors';
+// import { getAllStories } from 'src/app/ngrx/stories/stories.selector';
+
+// import * as boardActions from 'src/app/ngrx/boards/boards.actions';
+// import * as releaseAction from 'src/app/ngrx/releases/releases.action';
+// import * as epicAction from 'src/app/ngrx/epics/epics.actions';
+// import * as sprintsAction from 'src/app/ngrx/sprints/sprints.actions';
+// import * as storyActions from 'src/app/ngrx/stories/stories.actions';
+// // import * as epicAction from 'src/app/ngrx/epics/epics.actions'
+
+// @Component({
+//   selector: 'app-form-view',
+//   templateUrl: './form-view.component.html',
+//   styleUrls: ['./form-view.component.scss'],
+// })
+// export class FormViewComponent {
+//   formValue: String = '';
+//   entityOptions = Object.keys(formFields);
+//   currentFields: {
+//     name: string;
+//     type: string;
+//     optionsKey?: string;
+//     label: string;
+//   }[] = [];
+//   entityForm: FormGroup = this.fb.group({});
+
+//   entityForm: FormGroup;
+
+//   fieldSteps: {
+//     name: string;
+//     type: string;
+//     optionsKey?: string;
+//     label: string;
+//   }[][] = [];
+
+//   boardsLength: number = 0;
+//   releaseLength: number = 0;
+//   epicsLength: number = 0;
+//   storiesLength: number = 0;
+//   sprintsLength: number = 0;
+//   // Length: number = 0
+
+//   dropdownOptions: { [key: string]: { id: number; name: string }[] } = {
+//     boards: [
+//       { id: 1, name: 'Board Alpha' },
+//       { id: 2, name: 'Board Beta' },
+//     ],
+//     users: [
+//       { id: 101, name: 'Alice' },
+//       { id: 102, name: 'Bob' },
+//     ],
+//     sprints: [
+//       { id: 201, name: 'Sprint 1' },
+//       { id: 202, name: 'Sprint 2' },
+//     ],
+//     epics: [
+//       { id: 301, name: 'Epic A' },
+//       { id: 302, name: 'Epic B' },
+//     ],
+//   };
+
+//   constructor(private fb: FormBuilder, private store: Store) {
+//     this.entityForm = this.fb.group({
+//       entity: [''], // just one control: the selected entity type
+//     });
+
+//     this.entityForm = this.fb.group({});
+
+//     this.store.select(getAllBoards).subscribe((data) => {
+//       console.log('boards', data);
+//       this.boardsLength = data.boards.length;
+//     });
+//     this.store.select(getAllReleases).subscribe((data) => {
+//       console.log('releases', data);
+//       this.releaseLength = data.releases.length;
+//     });
+//     this.store.select(getAllEpics).subscribe((data) => {
+//       console.log('epics', data);
+//       this.epicsLength = data.epics.length;
+//     });
+//     this.store.select(getAllSprints).subscribe((data) => {
+//       console.log('sprints', data);
+//       this.sprintsLength = data.sprints.length;
+//     });
+//     this.store.select(getAllStories).subscribe((data) => {
+//       console.log('stories', data);
+//       this.storiesLength = data.stories.length;
+//     });
+//   }
+
+//   onEntityChange(event: Event | any) {
+//     // console.log("entity");
+
+//     const selectedValue = (event.target as HTMLSelectElement).value;
+//     this.formValue = selectedValue;
+
+//     console.log("entity",this.formValue);
+
+//     this.currentFields = formFields[selectedValue];
+//     const group: any = {};
+//     this.currentFields.forEach((field) => {
+//       group[field.name] = [''];
+//     });
+//     this.entityForm = this.fb.group(group);
+//   }
+//   //   [
+//   //     "Board",
+//   //     "Sprint",
+//   //     "Story",
+//   //     "Epic",
+//   //     "Release",
+//   //     "User"
+//   // ]
+
+//   splitFieldsIntoSteps(fields: any[], chunkSize: number): any[][] {
+//     const steps: any[][] = [];
+//     for (let i = 0; i < fields.length; i += chunkSize) {
+//       steps.push(fields.slice(i, i + chunkSize));
+//     }
+//     return steps;
+//   }
+
+//   // onEntityChange(event: any) {
+//   //   const selectedValue = event.value;
+//   //   this.formValue = selectedValue;
+//   //   this.currentFields = formFields[selectedValue];
+
+//   //   // split into steps
+//   //   this.fieldSteps = this.splitFieldsIntoSteps(this.currentFields, 2); // 2 fields per step
+
+//   //   const group: any = {};
+//   //   this.currentFields.forEach(field => {
+//   //     group[field.name] = [''];
+//   //   });
+//   //   this.entityForm = this.fb.group(group);
+//   // }
+
+//   onSubmit() {
+//     // this.entityForm.controls['BoardId'].setValue(this.boardsLength + 1)
+
+//     // console.log(this.entityForm.value, data);
+//     switch (this.formValue) {
+//       case 'Board': {
+//         let data = {
+//           ...this.entityForm.value,
+//           BoardId: this.boardsLength + 1,
+//         };
+//         this.store.dispatch(boardActions.addBoardSuccess({ board: data }));
+//         break;
+//       }
+//       case 'Release': {
+//         let data = {
+//           ...this.entityForm.value,
+//           ReleaseId: this.releaseLength + 1,
+//         };
+//         this.store.dispatch(releaseAction.addReleaseSuccess({ release: data }));
+//         break;
+//       }
+//       case 'Epic': {
+//         let data = {
+//           ...this.entityForm.value,
+//           EpicId: this.epicsLength + 1,
+//         };
+//         this.store.dispatch(epicAction.addEpicSuccess({ epic: data }));
+//         break;
+//       }
+//       case 'Sprint': {
+//         let data = {
+//           ...this.entityForm.value,
+//           SprintId: this.sprintsLength + 1,
+//         };
+//         this.store.dispatch(sprintsAction.addSprintSuccess({ sprint: data }));
+//         break;
+//       }
+//       case 'Story': {
+//         let data = {
+//           ...this.entityForm.value,
+//           StoryId: this.storiesLength + 1,
+//         };
+//         this.store.dispatch(storyActions.addStorySuccess({ story: data }));
+//         break;
+//       }
+//     }
+//   }
+
+//   boardForm!: FormGroup;
+
+//   ngOnInit(): void {
+//     this.boardForm = this.fb.group({
+//       boardName: ['', [Validators.required, Validators.maxLength(100)]]
+//     });
+//   }
+// }
+
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { formFields } from 'src/app/config/form-config';
+
+import * as boardActions from 'src/app/ngrx/boards/boards.actions';
+import * as releaseAction from 'src/app/ngrx/releases/releases.action';
+import * as epicAction from 'src/app/ngrx/epics/epics.actions';
+import * as sprintsAction from 'src/app/ngrx/sprints/sprints.actions';
+import * as storyActions from 'src/app/ngrx/stories/stories.actions';
 import { getAllBoards } from 'src/app/ngrx/boards/boards.selector';
 import { getAllReleases } from 'src/app/ngrx/releases/releases.selectors';
 import { getAllEpics } from 'src/app/ngrx/epics/epics.selectors';
 import { getAllSprints } from 'src/app/ngrx/sprints/sprints.selectors';
 import { getAllStories } from 'src/app/ngrx/stories/stories.selector';
-
-import * as boardActions from 'src/app/ngrx/boards/boards.actions'
-import * as releaseAction from 'src/app/ngrx/releases/releases.action'
-import * as epicAction from 'src/app/ngrx/epics/epics.actions'
-import * as sprintsAction from 'src/app/ngrx/sprints/sprints.actions'
-import * as storyActions from 'src/app/ngrx/stories/stories.actions'
-// import * as epicAction from 'src/app/ngrx/epics/epics.actions'
+import { BoardsInterface, SprintsInterface } from 'src/app/utils/types';
 
 @Component({
   selector: 'app-form-view',
   templateUrl: './form-view.component.html',
-  styleUrls: ['./form-view.component.scss']
+  styleUrls: ['./form-view.component.scss'],
 })
 export class FormViewComponent {
-  formValue: String = ''
-  entityOptions = Object.keys(formFields);
-  currentFields: { name: string; type: string; optionsKey?: string, label: string }[] = [];
-  dynamicForm: FormGroup = this.fb.group({});
-
+  entityTypes = ['Board', 'Sprint', 'Story', 'Epic', 'User', 'Release'];
+  selectedEntity = '';
+  typeForm: FormGroup;
   entityForm: FormGroup;
 
-  fieldSteps: { name: string; type: string; optionsKey?: string, label: string }[][] = [];
+  boardsLength: number = 0;
+  releaseLength: number = 0;
+  epicsLength: number = 0;
+  storiesLength: number = 0;
+  sprintsLength: number = 0;
 
-  boardsLength: number = 0
-  releaseLength: number = 0
-  epicsLength: number = 0
-  storiesLength: number = 0
-  sprintsLength: number = 0
-  // Length: number = 0
-
-  dropdownOptions: { [key: string]: { id: number; name: string }[] } = {
-    boards: [
-      { id: 1, name: 'Board Alpha' },
-      { id: 2, name: 'Board Beta' }
-    ],
-    users: [
-      { id: 101, name: 'Alice' },
-      { id: 102, name: 'Bob' }
-    ],
-    sprints: [
-      { id: 201, name: 'Sprint 1' },
-      { id: 202, name: 'Sprint 2' }
-    ],
-    epics: [
-      { id: 301, name: 'Epic A' },
-      { id: 302, name: 'Epic B' }
-    ]
-  };
+  boards: BoardsInterface[] = [];
+  release: number = 0;
+  epics: number = 0;
+  stories: number = 0;
+  sprints: SprintsInterface[] = [];
 
   constructor(private fb: FormBuilder, private store: Store) {
-    this.entityForm = this.fb.group({
-      entity: ['']  // just one control: the selected entity type
+    this.typeForm = this.fb.group({
+      entityType: ['', Validators.required],
     });
 
-    this.dynamicForm = this.fb.group({});
+    this.entityForm = this.fb.group({});
 
-    this.store.select(getAllBoards).subscribe(data => {
-      console.log("boards", data);
-      this.boardsLength = data.boards.length
-    })
-    this.store.select(getAllReleases).subscribe(data => {
-      console.log("releases", data);
-      this.releaseLength = data.releases.length
-    })
-    this.store.select(getAllEpics).subscribe(data => {
-      console.log("epics", data);
-      this.epicsLength = data.epics.length
-    })
-    this.store.select(getAllSprints).subscribe(data => {
-      console.log("sprints", data);
-      this.sprintsLength = data.sprints.length
-    })
-    this.store.select(getAllStories).subscribe(data => {
-      console.log("stories", data);
-      this.storiesLength = data.stories.length
-    })
-  }
-
-  onEntityChange(event: Event) {
-    const selectedValue = (event.target as HTMLSelectElement).value;
-    this.formValue = selectedValue
-    this.currentFields = formFields[selectedValue];
-    const group: any = {};
-    this.currentFields.forEach(field => {
-      group[field.name] = [''];
+    this.store.select(getAllBoards).subscribe((data) => {
+      console.log('boards', data);
+      this.boardsLength = data.boards.length;
     });
-    this.dynamicForm = this.fb.group(group);
+    this.store.select(getAllReleases).subscribe((data) => {
+      console.log('releases', data);
+      this.releaseLength = data.releases.length;
+    });
+    this.store.select(getAllEpics).subscribe((data) => {
+      console.log('epics', data);
+      this.epicsLength = data.epics.length;
+    });
+    this.store.select(getAllSprints).subscribe((data) => {
+      console.log('sprints', data);
+      this.sprintsLength = data.sprints.length;
+      this.sprints = data.sprints;
+    });
+    this.store.select(getAllStories).subscribe((data) => {
+      console.log('stories', data);
+      this.storiesLength = data.stories.length;
+    });
   }
-  //   [
-  //     "Board",
-  //     "Sprint",
-  //     "Story",
-  //     "Epic",
-  //     "Release",
-  //     "User"
-  // ]
 
-  splitFieldsIntoSteps(fields: any[], chunkSize: number): any[][] {
-    const steps: any[][] = [];
-    for (let i = 0; i < fields.length; i += chunkSize) {
-      steps.push(fields.slice(i, i + chunkSize));
+  onEntityTypeChange() {
+    this.selectedEntity = this.typeForm.value.entityType;
+    this.buildEntityForm();
+  }
+
+  buildEntityForm() {
+    const controls: any = {};
+
+    switch (this.selectedEntity) {
+      case 'Board':
+        controls['BoardName'] = ['', Validators.required];
+        break;
+
+      case 'Sprint':
+        controls['SprintName'] = ['', Validators.required];
+        controls['SprintNo'] = ['', Validators.required];
+        controls['SprintPoint'] = [''];
+        controls['StartDate'] = [''];
+        controls['EndDate'] = [''];
+        // controls['BoardId'] = [''];
+        // controls['ReleaseId'] = [''];
+        break;
+
+      case 'Story':
+        controls['StoryName'] = ['', Validators.required];
+        controls['Description'] = [''];
+        controls['Flag'] = [false];
+        controls['StatusId'] = [''];
+        controls['BoardId'] = [''];
+        controls['UserId'] = [''];
+        controls['SprintId'] = [''];
+        controls['EpicId'] = [''];
+        break;
+
+      case 'Epic':
+        controls['EpicName'] = ['', Validators.required];
+        controls['Description'] = [''];
+        break;
+
+      case 'User':
+        controls['UserName'] = ['', Validators.required];
+        controls['Email'] = ['', [Validators.required, Validators.email]];
+        controls['Password'] = ['', Validators.required];
+        controls['RoleId'] = [''];
+        break;
+
+      case 'Release':
+        controls['ReleaseName'] = ['', Validators.required];
+        controls['SprintId'] = ['', Validators.required];
+        break;
+
+      default:
+        // Optionally handle default case
+        break;
     }
-    return steps;
+
+    this.entityForm = this.fb.group(controls);
   }
 
-  // onEntityChange(event: any) {
-  //   const selectedValue = event.value;
-  //   this.formValue = selectedValue;
-  //   this.currentFields = formFields[selectedValue];
-
-  //   // split into steps
-  //   this.fieldSteps = this.splitFieldsIntoSteps(this.currentFields, 2); // 2 fields per step
-
-  //   const group: any = {};
-  //   this.currentFields.forEach(field => {
-  //     group[field.name] = [''];
-  //   });
-  //   this.dynamicForm = this.fb.group(group);
-  // }
+  submitForm() {
+    console.log('Submitted:', {
+      type: this.selectedEntity,
+      data: this.entityForm.value,
+    });
+  }
 
   onSubmit() {
-    // this.dynamicForm.controls['BoardId'].setValue(this.boardsLength + 1)
+    // this.entityForm.controls['BoardId'].setValue(this.boardsLength + 1)
 
-    // console.log(this.dynamicForm.value, data);
-    switch (this.formValue) {
+    console.log(this.entityForm.value);
+    switch (this.selectedEntity) {
       case 'Board': {
         let data = {
-          ...this.dynamicForm.value,
-          BoardId: (this.boardsLength + 1)
-        }
-        this.store.dispatch(boardActions.addBoardSuccess({ board: data }))
+          ...this.entityForm.value,
+          BoardId: this.boardsLength + 1,
+        };
+        this.store.dispatch(boardActions.addBoardSuccess({ board: data }));
         break;
       }
-      case "Release": {
+      case 'Release': {
         let data = {
-          ...this.dynamicForm.value,
-          ReleaseId: (this.releaseLength + 1)
-        }
-        this.store.dispatch(releaseAction.addReleaseSuccess({ release: data }))
+          ...this.entityForm.value,
+          ReleaseId: this.releaseLength + 1,
+        };
+        this.store.dispatch(releaseAction.addReleaseSuccess({ release: data }));
         break;
       }
-      case "Epic": {
+      case 'Epic': {
         let data = {
-          ...this.dynamicForm.value,
-          EpicId: (this.epicsLength + 1)
-        }
-        this.store.dispatch(epicAction.addEpicSuccess({ epic: data }))
+          ...this.entityForm.value,
+          EpicId: this.epicsLength + 1,
+        };
+        this.store.dispatch(epicAction.addEpicSuccess({ epic: data }));
         break;
       }
-      case "Sprint": {
+      case 'Sprint': {
         let data = {
-          ...this.dynamicForm.value,
-          SprintId: (this.sprintsLength + 1)
-        }
-        this.store.dispatch(sprintsAction.addSprintSuccess({ sprint: data }))
+          ...this.entityForm.value,
+          SprintId: this.sprintsLength + 1,
+        };
+        this.store.dispatch(sprintsAction.addSprintSuccess({ sprint: data }));
         break;
       }
-      case "Story": {
+      case 'Story': {
         let data = {
-          ...this.dynamicForm.value,
-          StoryId: (this.storiesLength + 1)
-        }
-        this.store.dispatch(storyActions.addStorySuccess({ story: data }))
+          ...this.entityForm.value,
+          StoryId: this.storiesLength + 1,
+        };
+        this.store.dispatch(storyActions.addStorySuccess({ story: data }));
         break;
       }
     }
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,58 +1,67 @@
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatToolbarModule } from "@angular/material/toolbar"
-import { MatSelectModule } from '@angular/material/select';;
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 export const matModules = [
-    MatSidenavModule,
-    MatIconModule,
-    MatButtonModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatListModule,
-    MatTableModule,
-    MatSelectModule,
-    MatStepperModule,
-    // MatFormFieldModule,
-    MatInputModule,
-    // MatButtonModule
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule
-]
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatListModule,
+  MatTableModule,
+  MatSelectModule,
+  MatStepperModule,
+  // MatFormFieldModule,
+  MatInputModule,
+  // MatButtonModule
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatCardModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+];
 
 export const card_colours = [
-    '#f4b8f4', '#9afc9a', '#9c9cfe', '#f8c971', '#f8f867', '#89dcf8'
-]
+  '#f4b8f4',
+  '#9afc9a',
+  '#9c9cfe',
+  '#f8c971',
+  '#f8f867',
+  '#89dcf8',
+];
 
 export const users = [
-    {
-        id: 1,
-        name: "manager",
-        role: "manager",
-        username: "manager",
-        password: "manager"
-    },
-    {
-        id: 2,
-        name: "team lead",
-        role: "team lead",
-        username: "lead1",
-        password: "lead"
-    },
-    {
-        id: 3,
-        name: "developer",
-        role: "developer",
-        username: "developer",
-        password: "developer"
-    }
-]
+  {
+    id: 1,
+    name: 'manager',
+    role: 'manager',
+    username: 'manager',
+    password: 'manager',
+  },
+  {
+    id: 2,
+    name: 'team lead',
+    role: 'team lead',
+    username: 'lead1',
+    password: 'lead',
+  },
+  {
+    id: 3,
+    name: 'developer',
+    role: 'developer',
+    username: 'developer',
+    password: 'developer',
+  },
+];
